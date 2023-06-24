b92d3d734f49b1f51b8a0487e25915627f74d093
adomovec <aleksej.domovec@xim.ag>
2023-06-24 16:27:55 +0200

    images changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['ad-d.info'], // Ersetzen Sie 'example.com' durch die Domains Ihrer Bilder
+    domains: ['ad-d.info','localhost:3000'], // Ersetzen Sie 'example.com' durch die Domains Ihrer Bilder
   },
 };
